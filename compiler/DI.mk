900956c8b1d8697487cf8e1ecf1f43653b722d14
galet.arthur <galet.arthur@gmail.com>
2024-03-15 09:05:39 +0100

    ajout des fichiers ignorés

---
+++ compiler/DI.mk
@@ -0,0 +1,4 @@
+ANTLRJAR=../antlr/jar/antlr-4.9.2-complete.jar
+ANTLRINC=../antlr/include
+ANTLRLIB=../antlr/lib/libantlr4-runtime.a
+ANTLR = java -cp $(ANTLRJAR) org.antlr.v4.Tool
---
+++ compiler/ubuntu.mk
@@ -0,0 +1,5 @@
+# the ANTLRJAR part below is copied from /usr/bin/antlr4
+ANTLRJAR=/usr/share/java/stringtemplate4.jar:/usr/share/java/antlr4.jar:/usr/share/java/antlr4-runtime.jar:/usr/share/java/antlr3-runtime.jar/:/usr/share/java/treelayout.jar
+ANTLRINC=/usr/include/antlr4-runtime
+ANTLRLIB=/usr/lib/x86_64-linux-gnu/libantlr4-runtime.a
+ANTLR=antlr4
