cfc831d1cff26649049e40e121428fa94ef9bf5c
Irvin Cote <irvincoteg@gmail.com>
2024-03-19 11:22:44 +0100

    Changing assembly generation for an IRInstr
    When generating the assembly instruction for a given IRInstr,
    the instruction parameters were directly placed in the assembly
    when we should have instead the transformation ofr that IR parameter
    to the target assembly with IR_reg_to_asm

--- compiler/IRInstr.cpp
+++ compiler/IRInstr.cpp
@@ -1,4 +1,5 @@
 #include "IRInstr.h"
+#include "BasicBlock.h"
 
 IRInstr::IRInstr(BasicBlock* bb_, Operation op, vector<string> params) {
     this->bb = bb_;
@@ -9,46 +10,46 @@ IRInstr::IRInstr(BasicBlock* bb_, Operation op, vector<string> params) {
 void IRInstr::gen_asm(ostream &o) {
     switch (op) {
         case ldconst:
-            o << "    movl    $" << params[1] << ", " << params[0] << "\n";
+            o << "    movl    $" << bb->cfg->IR_reg_to_asm(params[1]) << ", " << bb->cfg->IR_reg_to_asm(params[0]) << "\n";
             break;
         case copy:
-            o << "    movl    " << params[1] << ", %eax\n";
-            o << "    movl %eax, " << params[0] << "\n";
+            o << "    movl    " << bb->cfg->IR_reg_to_asm(params[1]) << ", %eax\n";
+            o << "    movl %eax, " << bb->cfg->IR_reg_to_asm(params[0]) << "\n";
             break;
         case add:
-            o << "    addl    " << params[1] << ", " << params[2] << "\n";
+            o << "    addl    " << bb->cfg->IR_reg_to_asm(params[1]) << ", " << bb->cfg->IR_reg_to_asm(params[2]) << "\n";
             break;
         case sub:
-            o << "    subl    " << params[1] << ", " << params[2] << "\n";
+            o << "    subl    " << bb->cfg->IR_reg_to_asm(params[1]) << ", " << bb->cfg->IR_reg_to_asm(params[2]) << "\n";
             break;
         case mul:
-            o << "    imull    " << params[1] << ", " << params[2] << "\n";
+            o << "    imull    " << bb->cfg->IR_reg_to_asm(params[1]) << ", " << bb->cfg->IR_reg_to_asm(params[2]) << "\n";
             break;
         case rmem:
-            o << "    movl    " << params[1] << ", %eax" << "\n";
-            o << "    movl    " << params[2] << "(%eax), " << params[0] << "\n";
+            o << "    movl    " << bb->cfg->IR_reg_to_asm(params[1]) << ", %eax" << "\n";
+            o << "    movl    " << bb->cfg->IR_reg_to_asm(params[2]) << "(%eax), " << bb->cfg->IR_reg_to_asm(params[0]) << "\n";
             break;
         case wmem:
-            o << "    movl    " << params[1] << ", %eax" << "\n";
-            o << "    movl    " << params[0] << ", " << params[2] << "(%eax)" << "\n";
+            o << "    movl    " << bb->cfg->IR_reg_to_asm(params[1]) << ", %eax" << "\n";
+            o << "    movl    " << bb->cfg->IR_reg_to_asm(params[0]) << ", " << bb->cfg->IR_reg_to_asm(params[2]) << "(%eax)" << "\n";
             break;
         case call:
-            o << "    call    " << params[0] << "\n";
+            o << "    call    " << bb->cfg->IR_reg_to_asm(params[0]) << "\n";
             break;
         case cmp_eq:
-            o << "    cmpl    " << params[1] << ", " << params[2] << "\n";
+            o << "    cmpl    " << bb->cfg->IR_reg_to_asm(params[1]) << ", " << bb->cfg->IR_reg_to_asm(params[2]) << "\n";
             o << "    sete    %al" << "\n";
-            o << "    movzbl    %al, " << params[0] << "\n";
+            o << "    movzbl    %al, " << bb->cfg->IR_reg_to_asm(params[0]) << "\n";
             break;
         case cmp_lt:
-            o << "    cmpl    " << params[1] << ", " << params[2] << "\n";
+            o << "    cmpl    " << bb->cfg->IR_reg_to_asm(params[1]) << ", " << bb->cfg->IR_reg_to_asm(params[2]) << "\n";
             o << "    setl    %al" << "\n";
-            o << "    movzbl    %al, " << params[0] << "\n";
+            o << "    movzbl    %al, " << bb->cfg->IR_reg_to_asm(params[0]) << "\n";
             break;
         case cmp_le:
-            o << "    cmpl    " << params[1] << ", " << params[2] << "\n";
+            o << "    cmpl    " << bb->cfg->IR_reg_to_asm(params[1]) << ", " << bb->cfg->IR_reg_to_asm(params[2]) << "\n";
             o << "    setle    %al" << "\n";
-            o << "    movzbl    %al << params[0]" << "\n";
+            o << "    movzbl    %al << bb->cfg->IR_reg_to_asm(params[0])" << "\n";
             break;
     }
 }
