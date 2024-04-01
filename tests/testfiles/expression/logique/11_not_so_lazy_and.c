ce356a225f6165693f2e8423336c89c01eb7624d
arosard <alexandre.rosard@insa-lyon.fr>
2024-04-01 19:22:13 +0200

    Correction de tests

--- tests/testfiles/expression/logique/11_not_so_lazy_and.c
+++ tests/testfiles/expression/logique/11_not_so_lazy_and.c
@@ -1,5 +1,5 @@
 int main() {
     int a;
-    int b = (a = 0) && (a = 3);
+    int b = (a = 3) && (a = 0);
     return a + b;
 }
--- tests/testfiles/functions/03_no_return.c
+++ tests/testfiles/functions/03_no_return.c
@@ -2,3 +2,7 @@ int no_return() {
     int a = 4 + 5;
 }
 
+int main(){
+    return 0;
+}
+
